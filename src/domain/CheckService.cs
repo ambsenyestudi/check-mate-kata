f4e702fbe54894a6a0a8bf2f507285fea5a6bd0e
ambsenyestudi <adrifre7@hotmail.com>
2022-05-21 19:43:33 +0200

    pawn kill king is only one square away

--- src/domain/CheckService.cs
+++ src/domain/CheckService.cs
@@ -1,5 +1,6 @@
 ﻿using Checkmate.Detector.Domain.Game;
 using System;
+using System.Linq;
 
 namespace Checkmate.Detector.Domain
 {
@@ -14,7 +15,32 @@ namespace Checkmate.Detector.Domain
 
         public bool IsCheck(GameId gameId)
         {
-            return gameRepository.GetBy(gameId) == GameLayout.Empty;
+            var gameLayout = gameRepository.GetBy(gameId);
+            var pieces = gameLayout.GetPieces();
+            var attackPiece = pieces.First();
+            if(IsPawn(attackPiece))
+            {
+                return GetDistance(pieces) == 1;
+            }
+            return false;
         }
+
+        private int GetDistance(string[] pieces)
+        {
+            var attackPiece = pieces.First();
+            var kingPiece = pieces.Last();
+            return (int)Math.Sqrt(
+                Math.Pow(GetColumnDistance(attackPiece, kingPiece), 2) +
+                Math.Pow(GetRowDistance(attackPiece, kingPiece), 2));
+        }
+
+        private double GetRowDistance(string attackPiece, string kingPiece) =>
+            attackPiece[2] - kingPiece[2];
+
+        private int GetColumnDistance(string attackPiece, string kingPiece) =>
+            attackPiece[1] - kingPiece[1];
+
+        private bool IsPawn(string attackPiece) =>
+            attackPiece.StartsWith("P");
     }
 }
