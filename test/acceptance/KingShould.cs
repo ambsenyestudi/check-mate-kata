2b1d65c13cd078f49d9a16f994e6e0ab123de328
ambsenyestudi <adrifre7@hotmail.com>
2022-05-22 12:12:32 +0200

    King check after movin at bishpo's range

--- test/acceptance/KingShould.cs
+++ test/acceptance/KingShould.cs
@@ -25,5 +25,18 @@ namespace Checkmate.Detector.Acceptance.Test
             gameService.TryMove(startPos, endPos, gameId);
             Assert.True(checkService.IsCheck(gameId));
         }
+
+        [Theory]
+        [InlineData("Kd8", "Bf7", "e8")]
+        [InlineData("Kd8", "Bb7", "c8")]
+        [InlineData("Kd8", "Bh3", "c8")]
+        [InlineData("Kd8", "Ba4", "e8")]
+        public void Detect_Check_When_Moved_At_Bishop_Range(string king, string bishop, string endPos)
+        {
+            var gameId = gameService.Load(bishop, king);
+            var startPos = king.Substring(1, 2);
+            gameService.TryMove(startPos, endPos, gameId);
+            Assert.True(checkService.IsCheck(gameId));
+        }
     }
 }
