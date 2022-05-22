7ed6bc4cc2c309e8c35e9ebc38227cdf04a0aed9
ambsenyestudi <adrifre7@hotmail.com>
2022-05-22 12:01:23 +0200

    Quen acceptance test completion

--- test/acceptance/QueenShould.cs
+++ test/acceptance/QueenShould.cs
@@ -16,11 +16,27 @@ namespace Checkmate.Detector.Acceptance.Test
         }
 
         [Theory]
+        [InlineData("Qa1", "Kd8", "c1")]
+        [InlineData("Qe1", "Kd8", "b4")]
+        public void Ignore_When_King_Not_At_Range(
+                    string knight, string king, string endPos)
+        {
+            var gameId = gameService.Load(knight, king);
+            var startPos = knight.Substring(1, 2);
+            gameService.TryMove(startPos, endPos, gameId);
+            Assert.False(checkService.IsCheck(gameId));
+        }
+
+        [Theory]
         [InlineData("Qa1", "Kd8", "d1")]
         [InlineData("Qa1", "Kd8", "a8")]
         [InlineData("Qh1", "Kd8", "d1")]
         [InlineData("Qh1", "Kd8", "h8")]
         [InlineData("Qa8", "Kd1", "d8")]
+        [InlineData("Qc1", "Kd8", "c7")]
+        [InlineData("Qe1", "Kd8", "e7")]
+        [InlineData("Qe1", "Kd8", "a5")]
+        [InlineData("Ba4", "Kd8", "h4")]
         public void Detect_Check_When_Moved_At_Killing_Range(string queen, string king, string endPos)
         {
             var gameId = gameService.Load(queen, king);
