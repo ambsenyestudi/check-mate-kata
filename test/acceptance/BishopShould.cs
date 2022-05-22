72db409b74ff77323dbcfadddd8c37ffd34b604e
ambsenyestudi <adrifre7@hotmail.com>
2022-05-22 12:20:23 +0200

    Bishop move then check acceptance criteria refactor

--- test/acceptance/BishopShould.cs
+++ test/acceptance/BishopShould.cs
@@ -16,14 +16,16 @@ namespace Checkmate.Detector.Acceptance.Test
         }
 
         [Theory]
-        [InlineData("Bc7", "Kd8")]
-        [InlineData("Be7", "Kd8")]
-        [InlineData("Ba5", "Kd8")]
-        [InlineData("Bh4", "Kd8")]
-        public void Detect_Check_When_King_At_Killing_Range(string bishop, string king)
+        [InlineData("Bh2", "Kd8", "c7")]
+        [InlineData("Bh4", "Kd8", "e7")]
+        [InlineData("Be1", "Kd8", "a5" )]
+        [InlineData("Be1", "Kd8", "h4")]
+        public void Detect_Check_When_King_At_Killing_Range(string bishop, string king, string endPos)
         {
 
             var gameId = gameService.Load(bishop, king);
+            var startPos = bishop.Substring(1, 2);
+            gameService.TryMove(startPos, endPos, gameId);
             Assert.True(checkService.IsCheck(gameId));
         }
     }
