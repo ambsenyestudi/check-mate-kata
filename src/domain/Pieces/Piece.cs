fcabde5a70046f7df5e5e630c7e810a552cd1a48
ambsenyestudi <adrifre7@hotmail.com>
2022-05-22 08:35:32 +0200

    Bishop green

--- src/domain/Pieces/Piece.cs
+++ src/domain/Pieces/Piece.cs
@@ -11,6 +11,9 @@ namespace Checkmate.Detector.Domain.Pieces
         internal bool IsPawn() =>
             Kind.Equals("P");
 
+        internal bool IsBishop() =>
+            Kind.Equals("B");
+
         public override string ToString() =>
             Kind + Position.ToString();
 
@@ -22,6 +25,10 @@ namespace Checkmate.Detector.Domain.Pieces
                 return other.Position.IsInFrontOf(Position) && 
                     other.Position.IsInDiagonalTo(Position) &&
                     Position.GetDistance(other.Position) == 1;
+            }
+            if (IsBishop())
+            {
+                return other.Position.IsInDiagonalTo(Position);
             }
             return false;
         }
